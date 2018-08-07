a16f32cd789504855c92391ddca487d2a52283a6
Joshua Gelbard <jgelbard@e1z4r13p3.42.us.org>
2018-08-06 21:17:24 -0700

    Add player and tile structs

--- server/inc/server.h
+++ server/inc/server.h
@@ -1,5 +1,29 @@
 
 #ifndef SERVER_H
 # define SERVER_H
+# include <stdio.h>
+# include <stdlib.h>
 
+typedef struct			s_tile
+{
+	struct s_player		*players;
+	int					num_players;
+	int					stones[6];
+	int					food;
+	int					x;
+	int					y;
+}						t_tile;
+
+typedef struct			s_player
+{
+	struct s_tile		*tile;
+	int					facing;
+	int					stones[6];
+	int					food;
+	int					energy;
+	int					level;
+	int					id;
+	int					team_id;
+	int					egg;
+}						t_player;
 #endif
