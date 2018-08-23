9dbae54ab6a9cc9274c4291132e3615e0f1dea0e
Josh Gelbard <jgelbard@student.42.us.org>
2018-08-23 11:17:10 -0700

    Fill in send_stringified_responses

--- server/src/send_stringified_responses.c
+++ server/src/send_stringified_responses.c
@@ -1,6 +1,20 @@
 #include "server.h"
+#include "command_list_type.h"
+#include "command_type.h"
+#include "client_type.h"
 
 void	send_stringified_responses(t_command_list *lst)
 {
-	return ;
+	t_client	*client;
+	t_command	*cmd;
+
+	while (lst)
+	{
+		cmd = lst->cmd;
+		client = get_client_by_player_id(cmd->player_id);
+		assert(client != NULL);
+		if (send(client->socket_fd, cmd->response, strlen(cmd->response), 0) <= 0)
+			perror("send stringified response");
+		lst = lst->next;
+	}
 }
