d3af6d17071478b2c2e0f504ef67c8c085a07be6
Josh Gelbard <jgelbard@student.42.us.org>
2018-09-08 12:22:28 -0700

    Remove commented out code

--- server/src/game/tile_data_api.c
+++ server/src/game/tile_data_api.c
@@ -3,25 +3,6 @@
 #include "tile_type.h"
 
 /*
-void		tile_get_data(t_vec pos, t_tile_data *ret)
-{
-	t_tile		*t;
-
-	t = &g_map->tile[pos.x][pos.y];
-	ret->pos.x = pos.x;
-	ret->pos.y = pos.y;
-	ret->stones[0] = t->stones[0];
-	ret->stones[1] = t->stones[1];
-	ret->stones[2] = t->stones[2];
-	ret->stones[3] = t->stones[3];
-	ret->stones[4] = t->stones[4];
-	ret->stones[5] = t->stones[5];
-	ret->food = t->food;
-	ret->num_players = t->num_players;
-}
-*/
-
-/*
 ** Takes pid to get reference to a tile.
 ** Puts the count of players on that tile in [ *count ]
 ** malloc's a new int[] with pids of players on the tile.
