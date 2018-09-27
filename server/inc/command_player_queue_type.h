5ce47aa5a9550862f385c2c7f36d038c4dd8dea2
regien <gerardomalpartidavela@gmail.com>
2018-09-26 21:17:13 -0700

    add forgotten file

---
+++ server/inc/command_player_queue_type.h
@@ -0,0 +1,14 @@
+#ifndef COMMAND_PLAYER_QUEUE_TYPE_H
+# define COMMAND_PLAYER_QUEUE_TYPE_H
+# include "server.h"
+# include "command_list_type.h"
+
+struct s_ply_cmd_queue
+{
+	t_command_list	*jobs[10];
+	int				head_ind;
+	int				size;
+	int				dequeue_timer;
+};
+
+#endif
