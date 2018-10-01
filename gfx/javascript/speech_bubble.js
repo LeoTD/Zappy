3af5b4654c69c288c5cae0347e30e0ce4a052afc
Josh Gelbard <jgelbard@student.42.us.org>
2018-09-30 20:23:19 -0700

    Dispose of action popup sprite when done animating

--- gfx/javascript/speech_bubble.js
+++ gfx/javascript/speech_bubble.js
@@ -1,33 +1,17 @@
 class speechBubble {
 	constructor(player) {
-
 		this.p = player;
-
-		this.s = {
-			put: undefined,
-			take: undefined,
-			inventory:	undefined,
-			broadcast:	undefined,
-			see: undefined,
-			kick: undefined,
-			incant: undefined,
-			incantFail:	undefined,
-			fork: undefined,
-			layEgg: undefined,
-			doneLayingEgg: undefined,
-			eggHatch:	undefined,
-			death: undefined,
-			fail: undefined,
-		};
 	}
 
 	cmdPopup(key) {
 		SpriteData[key].manager.renderingGroupId = 1;
-		this.s[key] = new BABYLON.Sprite(key, SpriteData[key].manager);
-		this.s[key].color.a = 1;
-		this.s[key].size = 5;
-		this.updatePosition(key);
-		this.s[key].isVisible = true;
+		const popup = new BABYLON.Sprite(key, SpriteData[key].manager);
+		popup.color.a = 1;
+		popup.size = 5;
+		for (let axis of ['x', 'y', 'z'])
+			popup.position[axis] = this.p.sprite.position[axis];
+		popup.position.y += this.p.sprite.size;
+		popup.isVisible = true;
 
 		const cascade = new BABYLON.Animation("cascadePopup",
 			'position.y',
@@ -35,7 +19,7 @@ class speechBubble {
 			BABYLON.Animation.ANIMATIONTYPE_FLOAT,
 			BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT
 		);
-		var start = this.s[key].position.y;
+		var start = popup.position.y;
 		var end = start / 2;
 		cascade.setKeys([
 			{ frame: 0, value: start },
@@ -59,24 +43,15 @@ class speechBubble {
 		]);
 
 		game.scene.beginDirectAnimation(
-			this.s[key],
+			popup,
 			[cascade, fade],
 			0,
 			100,
 			false,
 			1,
 			() => {
-				this.s[key].dispose();
+				popup.dispose();
 			}
 		);
 	}
-
-	updatePosition(key) {
-		this.s[key].position.x = this.p.sprite.position.x;
-		this.s[key].position.y = this.p.sprite.position.y;
-		this.s[key].position.z = this.p.sprite.position.z;
-
-		this.s[key].position.y += this.p.sprite.size;
-	}
-
 }
